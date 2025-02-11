5c326d8f77149f020af599e85e3aac37e721945f
JTR254 <jackrindge@gmail.com>
2025-02-11 16:06:26 -0500

    Task 1

---
+++ in_class_assignment_2.js
@@ -0,0 +1,12 @@
+// Task 1
+
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Desk Chair", price: 150, category: "Furniture"},
+];
+
+// function getProductsByCategory(products, category_name) {
+//     return products.filter(product => product.category === category_name);
+// };
+
+// console.log(getProductsByCategory(products, "Electronics"));
